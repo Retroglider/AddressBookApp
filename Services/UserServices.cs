96b33011259411ef19c68d146c6695f56a0e453e
Rhyss Leary <rhyss@link2.com>
2019-05-16 21:40:23 -0700

    Removed WebApplication project.

---
+++ Services/UserServices.cs
@@ -0,0 +1,19 @@
+﻿using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class UserServices
+    {
+        private readonly UserRepository _userRepository;
+        public UserServices(IUserRepository userRepository)
+        {
+
+        }
+
+    }
+}
